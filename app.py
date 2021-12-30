0f0224cf2c0bddb09ce2886081e7f4dfbcdbffe7
Spencer Hurrle <spencer.hurrle@gmail.com>
2021-12-29 19:39:37 -0500

    Figured out dictionary looping in Flask

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from helper import pets
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return '''
+  <h1>Adopt a Pet!</h1>
+  <p>Browse through the links below to find your new furry friend:</p>
+  <ul>
+  <li><a href="/animals/dogs">Dogs</a></li>
+  <li><a href="/animals/cats">Cats</li>
+  <li><a href="/animals/rabbits">Rabbits</li>
+  </ul>
+  '''
+
+@app.route('/animals/<pet_type>')
+def animals(pet_type):
+  html =  f'<h1>List of {pet_type}</h1>'
+  html+='<ul>'
+  for pet in pets[pet_type]:
+    html+=f'<li>{pet['name']}</li>'
+  html+='</ul>'
+  return html
